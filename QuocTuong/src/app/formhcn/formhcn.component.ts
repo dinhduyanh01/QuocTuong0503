83d0f2d2a3b0da94d37e8850c67675201b28fd59
nguyentuong <tuong0503vn@gmail.com>
2019-10-02 12:53:22 +0700

    dsáda
    dsád

--- QuocTuong/src/app/formhcn/formhcn.component.ts
+++ QuocTuong/src/app/formhcn/formhcn.component.ts
@@ -6,6 +6,30 @@ import { FormsModule } from '@angular/forms';
   styleUrls: ['./formhcn.component.css']
 })
 export class FormhcnComponent implements OnInit {
+  luong = {
+    giatri : 2000001
+  }
+  dulieu = [
+    {
+      name: "Nước ngọt",
+      price: 10000,
+      thanhtien: null,
+      soluong: null
+    },
+    {
+      name: "Cà phê đen",
+      price: 12000,
+      thanhtien: null,
+      soluong: null
+    },
+    {
+      name: "Thuốc lá",
+      price: 18000,
+      thanhtien: null,
+      soluong: null
+    },
+  ];
+  soluong1: 1;
   a = null
   thuongtet = 0;
   hcn = {
@@ -24,12 +48,12 @@ export class FormhcnComponent implements OnInit {
   tuoithuong = [{
     age: "dưới 25",
     giatri: 0.07,
-  }, 
+  },
   {
     age: "25 đến 40",
     giatri: 0.1,
   },
-   {
+  {
     age: "trên 40",
     giatri: 0.15,
   }]
@@ -46,9 +70,30 @@ export class FormhcnComponent implements OnInit {
     if (this.thuong.gioitinh == "Nu") {
       return this.thuongtet = this.thuong.luong * this.thuong.rate + 200000
     }
-    else
-    {
-      return this.thuongtet =this.thuong.luong*this.thuong.rate
+    else {
+      return this.thuongtet = this.thuong.luong * this.thuong.rate
     }
   }
-}
+  tinhtien() {
+    for (var i = 0; i < this.dulieu.length; i++) {  
+      for (var j = 0; j < this.dulieu.length; j++) {
+        return this.dulieu[i].thanhtien = this.soluong1 * this.dulieu[i].thanhtien 
+      }
+      
+    }
+  }
+  thongbao(){
+    if(this.luong.giatri >= 2000000)
+    {
+      return this.luong.giatri
+    }
+    else
+    { 
+     
+      alert("nhập sai")
+    }
+     
+    }
+  }
+
+
