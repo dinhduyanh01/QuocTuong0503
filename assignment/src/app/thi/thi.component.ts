df1fe8f2c005eb173fec9232baf93632adaba6bb
dinhduyanh01 <54561476+dinhduyanh01@users.noreply.github.com>
2019-11-16 21:59:32 +0700

    xấd
    âs

--- assignment/src/app/thi/thi.component.ts
+++ assignment/src/app/thi/thi.component.ts
@@ -1,45 +1,42 @@
 import { Component, OnInit } from '@angular/core';
-import{ActivatedRoute}from '@angular/router'
-import{DataService} from '../DAL/data.service';
-
+import { ActivatedRoute } from '@angular/router'
+import { DataService } from '../DAL/data.service';
+import{NgxPaginationModule} from 'ngx-pagination';
 @Component({
   selector: 'app-thi',
   templateUrl: './thi.component.html',
   styleUrls: ['./thi.component.css']
 })
 export class ThiComponent implements OnInit {
-  p;
-  quiz : any
-  id :  any
-  name : String
-  list : any
-  product : any
-  monthi : any
-  url : String
-  constructor(private datta : DataService , private route : ActivatedRoute) { }
+  p=1;
+  quiz: any
+  id: any
+  name: String
+  list: any
+  product: any
+  monthi: any
+  url: String
+  constructor(private datta: DataService, private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.route.paramMap.subscribe(para => {
-      this.id = +para.get("Id")
+      this.id = para.get("Id")
+      this.url = "../assets/Quizs/" + this.id + ".json"
     });
-    this.url = 'assets/Quizs/' + this.id + '.json'
-    this.datta.getQuiz(this.url).subscribe((dulieu) =>
-    {
+
+    this.datta.getQuiz(this.url).subscribe((dulieu) => {
       this.quiz = dulieu
     })
   }
-  nextpage(){
-    if(this.quiz.length/ 4 > this.p )
-    {
-       return this.p ++;
+  nextpage() {
+    if (this.quiz.length > this.p) {
+      return this.p++;
     }
-}
-backpage()
-{
-    if(this.p >0)
-    {
-      return this.p --;
+  }
+  backpage() {
+    if (this.p > 0) {
+      return this.p--;
     }
-}
+  }
 
 }
